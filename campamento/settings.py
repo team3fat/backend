555c28846269694f67690960821cc745bb086732
Diego <d.gimenez0101@gmail.com>
2019-06-18 10:04:49 -0300

    Modificado para aceptar CORS

--- campamento/settings.py
+++ campamento/settings.py
@@ -104,8 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 10
-
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'PAGE_SIZE': 10,
 }
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
