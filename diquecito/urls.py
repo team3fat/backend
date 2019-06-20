2bf378c5039988899dee4036b3d27389c1da08f2
TomasTurM <tomy.monier@hotmail.com>
2019-06-20 14:27:01 -0300

    Revert "Develop - Feature 1 Merge"

--- diquecito/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from diquecito.views import *
-
-urlpatterns = [
-    path('usuario/', UsuarioList.as_view()),
-    path('post/', PostList.as_view()),
-    path('reservation/', ReservationList.as_view()),
-    path('qualification/', QualificationList.as_view())
-]
