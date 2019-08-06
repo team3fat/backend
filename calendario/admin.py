762b32988ae7f3cd0a2c0974d41ec22136124d1b
gabrieldalbo12 <gabrieldalbo2@gmail.com>
2019-08-06 11:56:37 -0300

    models.py actualizado

--- calendario/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from calendario.models import *
-# Register your models here.
--- calendario/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from calendario.models import *
-# Create your views here.
-
-
-
